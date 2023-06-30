649e2cd5289dc6317c1b253c61345637f6b8860d
buffpsi <rlawlstjd778@naver.com>
2023-06-30 13:02:22 +0900

    init : match part set up

--- src/main/java/com/matchUpSports/boundedContext/match/controller/MatchController.java
+++ src/main/java/com/matchUpSports/boundedContext/match/controller/MatchController.java
@@ -1,8 +1,15 @@
 package com.matchUpSports.boundedContext.match.controller;
 
+import com.matchUpSports.base.rsData.RsData;
+import com.matchUpSports.boundedContext.match.entity.Match;
+import com.matchUpSports.boundedContext.match.matchFormDto.MatchForm;
 import com.matchUpSports.boundedContext.match.service.MatchService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +18,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MatchController {
     private final MatchService matchService;
 
+    //매치 페이지의 메인 조건 보여주기
+    @GetMapping("/filter")
+    public String showMatchingFilter() {
+        return "matching/filterPage";
+    }
+
+    @PostMapping
+    public String addMatch(@ModelAttribute MatchForm matchForm, Model model) {
+        RsData<Match> result = matchService.createMatch(matchForm);
+        if (result.isSuccess()) {
+            return "redirect:/match/waiting";
+        } else {
+            model.addAttribute("message", result.getMsg());
+            return "matching/filterPage"; //
+        }
+    }
+
+    //웨이팅 페이지 작업해야함
+    @GetMapping("/waiting")
+    public String showWaiting() {
+        return "matching/waiting";
+    }
 }
+
--- src/main/java/com/matchUpSports/boundedContext/match/entity/Match.java
+++ src/main/java/com/matchUpSports/boundedContext/match/entity/Match.java
@@ -1,31 +1,43 @@
 package com.matchUpSports.boundedContext.match.entity;
 
-import com.matchUpSports.boundedContext.field.entity.Field;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Getter
 @Setter
+@Builder
+@Table(name = "match_table") // 테이블 이름 변경
+@EntityListeners(AuditingEntityListener.class) // 추가이유 : createdDate Db에 생성하게함
 public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreatedDate
+    private LocalDateTime applicationDate;   // 신청일
+
     private String matchTier;       // 매치 티어
 
     private LocalDate matchDate;    // 매치 날짜
 
-    private int usageTime;          // 이용 시간
+    private Integer usageTime;          // 이용 시간
 
     private String progressStatus;  // 진행 상태 (타입에 관해서 추후에 생각해봐야 함)
 
-    private LocalDateTime applicationDate;   // 신청일
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    private Field field;                    // 시설
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private Field field;                    // 시설
+    //아래의 4가지 데이터는 field에서 받아와야할 데이터 입니다
+    private String fieldLocation; // 구장 위치 = fieldLocation
+    private String stadium; //구장 이름 = fieldName
+    private int subStadiumCount; //그 구장에 있는 구장 수 = courtCount
+    private int participantCount; //한 매치에 참여하는 인원수
 }
