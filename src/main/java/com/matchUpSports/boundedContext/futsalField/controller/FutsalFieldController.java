b36ab693a25f77ced4a6934528114439855c0590
SonHanSol1 <gksthf7869@naver.com>
2023-07-02 23:10:51 +0900

    feat: 시설 자세히 보기 틀 제작

--- src/main/java/com/matchUpSports/boundedContext/futsalField/controller/FutsalFieldController.java
+++ src/main/java/com/matchUpSports/boundedContext/futsalField/controller/FutsalFieldController.java
@@ -11,6 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -33,6 +34,12 @@ public class FutsalFieldController {
     public String showAdmMain2() {
         return "redirect:/field";
     }
+    @GetMapping("/detail/{id}")
+    public String showDetailFutsalField(@PathVariable Long id, Model model){
+
+        return "field/futsalFieldDetail";
+    }
+
 
     @GetMapping("/create")
     public String createField(Model model) {
