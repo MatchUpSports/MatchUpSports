90f40a5ea196ee19ed23f4320c21772266299bb1
SonHanSol1 <gksthf7869@naver.com>
2023-07-02 21:41:17 +0900

    design: 시설관리자 메인 페이지 제작

---
+++ src/main/java/com/matchUpSports/boundedContext/futsalField/repository/FieldImageRepository.java
@@ -0,0 +1,7 @@
+package com.matchUpSports.boundedContext.field.repository;
+
+import com.matchUpSports.boundedContext.field.entity.FieldImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FieldImageRepository extends JpaRepository<FieldImage, Long> {
+}
