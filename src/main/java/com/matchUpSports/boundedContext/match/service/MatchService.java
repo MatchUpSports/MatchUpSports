e46dfb22b76783a158d34433d0cbd442c58c1283
buffpsi <rlawlstjd778@naver.com>
2023-07-03 14:49:53 +0900

    fix: courtCount가 2개 구장 이상으로 등록되면 subStadium증가하도록 수정, filterPages 위치 재배치

--- src/main/java/com/matchUpSports/boundedContext/match/service/MatchService.java
+++ src/main/java/com/matchUpSports/boundedContext/match/service/MatchService.java
@@ -1,6 +1,8 @@
 package com.matchUpSports.boundedContext.match.service;
 
 import com.matchUpSports.base.rsData.RsData;
+import com.matchUpSports.boundedContext.field.entity.Field;
+import com.matchUpSports.boundedContext.field.repository.FieldRepository;
 import com.matchUpSports.boundedContext.match.entity.Match;
 import com.matchUpSports.boundedContext.match.matchFormDto.MatchForm;
 import com.matchUpSports.boundedContext.match.repository.MatchRepository;
@@ -9,7 +11,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 @Slf4j
@@ -18,6 +22,7 @@ import java.util.Objects;
 @Transactional(readOnly = true)
 public class MatchService {
     private final MatchRepository matchRepository;
+    private final FieldRepository fieldRepository;
 
     @Transactional
     public RsData<Match> createMatch(MatchForm matchForm) {
@@ -25,7 +30,6 @@ public class MatchService {
             return RsData.of("F-1", "입력한 정보 중 누락된 값이 있습니다.");
         }
 
-        // 사용자가 선택한 구장, 날짜, 시간으로 이미 등록된 매치 찾기
         List<Match> matches = matchRepository.findByStadiumAndMatchDateAndUsageTime(
                 matchForm.getStadium(),
                 matchForm.getMatchDate(),
@@ -33,45 +37,47 @@ public class MatchService {
         );
 
         int maxMemberCountForSubStadium = 2;
-        int maxSubStadiumCount = 2;
+        Field selectedField = fieldRepository.findByFieldName(matchForm.getStadium());
+        int maxSubStadiumCount = selectedField.getCourtCount();
 
-        // 등록된 매치가 있는 경우
+        Map<Integer, Integer> subStadiumMembers = new HashMap<>();
+        for (int i = 1; i <= maxSubStadiumCount; i++) {
+            subStadiumMembers.put(i, 0);
+        }
+
+        for (Match existingMatch : matches) {
+            int currentSubStadium = existingMatch.getSubStadiumCount();
+            int currentParticipantCount = existingMatch.getParticipantCount();
+            subStadiumMembers.put(currentSubStadium, currentParticipantCount);
+        }
+
+        int availableSubStadium = -1;
+        for (int i = 1; i <= maxSubStadiumCount; i++) {
+            if (subStadiumMembers.get(i) < maxMemberCountForSubStadium) {
+                availableSubStadium = i;
+                break;
+            }
+        }
+
+        if (availableSubStadium == -1) {
+            return RsData.of("F-1", "모든 구장이 가득 찼습니다.");
+        }
+
+        Match newMatch = matchForm.toEntity();
+        newMatch.setParticipantCount(1);
+        newMatch.setSubStadiumCount(availableSubStadium);
+
         if (!matches.isEmpty()) {
             for (Match existingMatch : matches) {
-                // subStadium의 인원이 가득 차지 않았으면 기존 매치에 참여
-                if (existingMatch.getParticipantCount() < maxMemberCountForSubStadium) {
+                if (existingMatch.getSubStadiumCount() == availableSubStadium) {
                     existingMatch.setParticipantCount(existingMatch.getParticipantCount() + 1);
-
-                    // 인원이 가득 찼으면 progressStatus를 업데이트
-                    if (existingMatch.getParticipantCount() == maxMemberCountForSubStadium) {
-                        existingMatch.setProgressStatus("1");
-                    }
-
                     matchRepository.save(existingMatch);
                     return RsData.successOf(existingMatch);
                 }
             }
-
-            // 모든 subStadium이 가득 차 있다면 새로운 매치 생성
-            if (matches.size() < maxSubStadiumCount) {
-                Match match = matchForm.toEntity();
-                match.setSubStadiumCount(matches.get(0).getSubStadiumCount() + 1);
-                match.setParticipantCount(1);
-
-                matchRepository.save(match);
-                return RsData.successOf(match);
-            } else {
-                // 모든 subStadium이 가득 찼을 때 에러 메시지 반환
-                return RsData.of("F-1", "모든 구장이 가득 찼습니다.");
-            }
         }
 
-        // 등록된 매치가 없는 경우, 새로운 매치를 생성하고 저장
-        Match match = matchForm.toEntity();
-        match.setSubStadiumCount(1);
-        match.setParticipantCount(1);
-
-        matchRepository.save(match);
-        return RsData.successOf(match);
+        matchRepository.save(newMatch);
+        return RsData.successOf(newMatch);
     }
 }
