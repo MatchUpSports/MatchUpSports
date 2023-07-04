fef9b438a611dc2bb3dc5989dbdd7943d1672051
Jihn0118 <lantern50@tukorea.ac.kr>
2023-07-04 10:10:41 +0900

    feat: 토스페이먼츠 API 연동

---
+++ src/main/java/com/matchUpSports/boundedContext/payment/service/PayService.java
@@ -0,0 +1,12 @@
+
+package com.matchUpSports.boundedContext.payment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PayService {
+    public void show(){
+
+    }
+}
+
