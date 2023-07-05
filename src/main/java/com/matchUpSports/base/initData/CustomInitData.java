5ee70f7b448dd857a122c177217ab085d7178713
SonHanSol1 <gksthf7869@naver.com>
2023-07-05 22:10:50 +0900

    feat: toastr 적용

--- src/main/java/com/matchUpSports/base/initData/CustomInitData.java
+++ src/main/java/com/matchUpSports/base/initData/CustomInitData.java
@@ -3,6 +3,7 @@ package com.matchUpSports.base.initData;
 import com.matchUpSports.base.Role;
 import com.matchUpSports.boundedContext.futsalField.entity.FutsalField;
 import com.matchUpSports.boundedContext.futsalField.repository.FutsalFieldRepository;
+import com.matchUpSports.boundedContext.futsalField.service.FutsalFieldService;
 import com.matchUpSports.boundedContext.member.entity.Member;
 import com.matchUpSports.boundedContext.member.repository.MemberRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 
@@ -18,31 +20,34 @@ import java.time.LocalTime;
 @Configuration
 public class CustomInitData {
     @Bean
-    CommandLineRunner initData(MemberRepository memberRepository, FutsalFieldRepository fieldRepository) {
+    CommandLineRunner initData(MemberRepository memberRepository, FutsalFieldRepository fieldRepository, FutsalFieldService futsalFieldService) {
 
         return new CommandLineRunner() {
 
             @Override
             @Transactional
             public void run(String... args) throws Exception {
+//
+//                Member user = Member.builder()
+//                        .username("user1")
+//                        .phoneNumber("010-1111-1111")
+//                        .email("user1@naver.com")
+//                        .build();
+//                user.addRole(Role.MANAGE);
+//                memberRepository.save(user);
 
-                Member user = Member.builder()
-                        .username("user1")
-                        .phoneNumber("010-1111-1111")
-                        .email("user1@naver.com")
-                        .build();
-                user.addRole(Role.USER);
-                memberRepository.save(user);
+//                FutsalField futsalField = FutsalField.builder()
+//                        .member(user)
+//                        .fieldName("아디다스 더 베이스 서울")
+//                        .fieldLocation("서울 용산구")
+//                        .price(100000)
+//                        .openTime(LocalTime.of(9, 0))
+//                        .closeTime(LocalTime.of(22, 0))
+//                        .courtCount(10)
+//                        .registNum("A")
+//                        .build();
+//                fieldRepository.save(futsalField);
 
-                fieldRepository.save(FutsalField.builder()
-                        .fieldName("아디다스 더 베이스 서울")
-                        .fieldLocation("서울 용산구")
-                        .price(100000)
-                        .openTime(LocalTime.of(9, 0))
-                        .closeTime(LocalTime.of(22, 0))
-                        .courtCount(10)
-                        .registNum("A")
-                        .build());
             }
         };
     }
