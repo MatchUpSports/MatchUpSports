60b076715f4d1c211f6faa0ad3248b71429f61b3
SonHanSol1 <gksthf7869@naver.com>
2023-07-05 22:06:46 +0900

    feat: 시설 목록 및 시설 등록 추가

--- src/main/java/com/matchUpSports/base/rsData/RsData.java
+++ src/main/java/com/matchUpSports/base/rsData/RsData.java
@@ -8,31 +8,19 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class RsData<T> {
-    private String resultCode;
+    private Status statusCode;
     private String msg;
-    private T data;
+    private T content;
 
-    public static <T> RsData<T> of(String resultCode, String msg, T data) {
-        return new RsData<>(resultCode, msg, data);
-    }
-
-    public static <T> RsData<T> of(String resultCode, String msg) {
-        return of(resultCode, msg, null);
-    }
-
-    public static <T> RsData<T> successOf(T data) {
-        return of("S-1", "성공", data);
-    }
-
-    public static <T> RsData<T> failOf(T data) {
-        return of("F-1", "실패", data);
+    public static <T> RsData<T> of(Status statusCode, String msg, T data) {
+        return new RsData<>(statusCode, msg, data);
     }
 
     public boolean isSuccess() {
-        return resultCode.startsWith("S-");
+        return statusCode.equals(Status.SUCCESS);
     }
 
-    public boolean isFail() {
-        return !isSuccess();
+    public enum Status {
+        SUCCESS, FAIL
     }
 }
