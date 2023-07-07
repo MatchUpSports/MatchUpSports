4dd06843e66c9d3c2ee83b44bef53947cc8c3d51
kgpksh <tkdgns543@gmail.com>
2023-07-07 22:29:53 +0900

    Feat : 배포를 위한 application.yml계열 정리 및 빌드 오류 해결을 위한 gradle-wrapper.jar 추가 application-prod, dev, test로 분화 test는 h2 인메모리로 작동하게 하고 테스트도 그에 맞게 변경

--- src/test/java/com/matchUpSports/MatchUpSportsApplicationTests.java
+++ src/test/java/com/matchUpSports/MatchUpSportsApplicationTests.java
@@ -2,8 +2,10 @@ package com.matchUpSports;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MatchUpSportsApplicationTests {
 
     @Test
