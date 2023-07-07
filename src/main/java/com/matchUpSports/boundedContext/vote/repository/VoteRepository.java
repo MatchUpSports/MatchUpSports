3ec0d25430fdb8c2a23b4a7cd111989da9560a3a
Jihn0118 <lantern50@tukorea.ac.kr>
2023-07-07 10:52:03 +0900

    feat: MVP 투표 기능

--- src/main/java/com/matchUpSports/boundedContext/vote/repository/VoteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.matchUpSports.boundedContext.vote.repository;
-
-import com.matchUpSports.boundedContext.vote.entity.Vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VoteRepository extends JpaRepository<Vote, Long> {
-}
