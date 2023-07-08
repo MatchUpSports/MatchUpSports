da060afa987bd4cdc57bbf97fd9c1c6237b3ebb1
kgpksh <107348564+kgpksh@users.noreply.github.com>
2023-07-08 15:45:33 +0900

    Feat : 배포를 위한 application.yml계열 정리 및 빌드 오류 해결을 위한 gradle-wrapper.jar 추가 (#10)
    application-prod, dev, test로 분화 test는 h2 인메모리로 작동하게 하고 테스트도 그에 맞게 변경

--- src/test/java/com/matchUpSports/boundedContext/member/entity/MemberTest.java
+++ src/test/java/com/matchUpSports/boundedContext/member/entity/MemberTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
+@ActiveProfiles("test")
 class MemberTest {
     @Autowired
     MemberRepository memberRepository;
