aa0bd16af0f03769dd581a06dc4bf326c1ac7f47
SonHanSol1 <gksthf7869@naver.com>
2023-07-10 13:42:28 +0900

    bug: 충돌 해결

--- src/main/java/com/matchUpSports/boundedContext/futsalField/repository/FutsalFieldRepository.java
+++ src/main/java/com/matchUpSports/boundedContext/futsalField/repository/FutsalFieldRepository.java
@@ -10,17 +10,12 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FutsalFieldRepository extends JpaRepository<FutsalField, Long> {
-<<<<<<< HEAD
     // FieldRepository.java
     List<FutsalField> findByFieldLocation(String location);
 
     FutsalField findByFieldName(String fieldName);
-
-    List<FutsalField> findByMember(Member member);
-=======
     Optional<FutsalField> findByIdAndDeleteDateIsNull(Long id);
 
     @Query("SELECT f FROM FutsalField f WHERE f.member =:member AND f.deleteDate IS NULL")
     List<FutsalField> findByMember(@Param("member") Member member);
->>>>>>> 7599b97 (feat: 시설 수정 삭제 구현 및 baseEntity 추가)
 }
